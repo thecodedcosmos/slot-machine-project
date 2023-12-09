6b5431c5d0773fd27e90215ade1c98f139e1eb8d
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-12-09 11:34:44 -0800

    Created my global variables for reel and slot

--- project.js
+++ project.js
@@ -18,6 +18,25 @@
 //Assign a variable. 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+
 const deposit = () => {
     while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
