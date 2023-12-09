df11fdbd0acfb7307cab63f606f5deca44a2465a
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-12-09 00:53:51 -0800

    added a function to determine which line to bet on.

--- project.js
+++ project.js
@@ -31,5 +31,17 @@ const deposit = () => {
   }
 };
 
+const getNumberOfLines = () => {
+    while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines= parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines <= 3 {
+        console.log("Invalid deposit amount, try again.");
+    } else {
+        return numberOfLines;
+    }
+  }
+}
+
 const depositAmount =  deposit();
-console.log(depositAmount);
