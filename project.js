214d6353f2aeb65f7672ed4d671d15bb418a3b45
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-12-11 20:04:36 -0800

    added reels function

--- project.js
+++ project.js
@@ -85,6 +85,21 @@ const spin = () => {
     }
 };
 
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1); 
+        }
+    }
+    
+    return reels;
+
+const reels = spin();
+console.log(reels);
 let balance =  deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
