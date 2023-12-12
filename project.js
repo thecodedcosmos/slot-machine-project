4f5311f141866c90c83b9637d46a9c139e842107
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-12-11 20:18:10 -0800

    added needed semicolons

--- project.js
+++ project.js
@@ -25,15 +25,15 @@ const SYMBOLS_COUNT = {
     "A": 2,
     "B": 4,
     "C": 6,
-    "D": 8
-}
+    "D": 8,
+};
 
 const SYMBOL_VALUES = {
     "A": 5,
     "B": 4,
     "C": 3,
-    "D": 2
-}
+    "D": 2,
+};
 
 
 
